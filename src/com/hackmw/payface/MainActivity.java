d9c9293fd8531a754b277d5f3d9988e756a2499a
DeveloperMeier <colbymeier1337@gmail.com>
2014-07-19 19:19:48 -0500

    Some files missed in last commit

--- src/com/hackmw/payface/MainActivity.java
+++ src/com/hackmw/payface/MainActivity.java
@@ -1,8 +1,22 @@
 package com.hackmw.payface;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import android.app.Activity;
 import android.app.ActionBar;
@@ -11,6 +25,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.JsonReader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -64,22 +79,72 @@ public class MainActivity extends Activity {
     }
     
     
+    
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
-                    String contents = data.getStringExtra("SCAN_RESULT"); //this is the result
+                    final String contents = data.getStringExtra("SCAN_RESULT"); //this is the result
                     TextView t = (TextView)findViewById(R.id.textView2);
 //                    t.setText(contents);
-                    //make the request
-                    //recieve request as json
-                    try {
-						JSONObject json = (JSONObject) new JSONParser().parse(contents);//_response);
-						t.setText("Payment successfully recieved from TESTCUSTOMER1 for $36.57");
-					} catch (ParseException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+                 // HTTP
+                    String url = "";//http://payface.cfapps.io/requestPayment.json?userId=TESTMERCHANT1&paymentToken=" + contents + "&amountUSD=5000";
+                    InputStream is = null;
+                    String result = "";
+            		try {	    	
+
+            			AsyncHttpClient client = new AsyncHttpClient();
+            			client.get(url, new AsyncHttpResponseHandler() {
+            				TextView t = (TextView)findViewById(R.id.textView2);
+            				
+            			    @Override
+            			    public void onStart() {
+            			        // called before request is started
+            			    }
+
+            			    @Override
+            			    public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+            			        // called when response HTTP status is "200 OK"
+            			    	JSONObject jsonObject = null;
+            			    	String responseString = null;
+								try {
+									responseString = new String(response);
+									Log.v("dbg", responseString);
+									jsonObject = new JSONObject(responseString);
+								} catch (Exception e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+            			    	String rtnMsg;
+            			    	try {
+									rtnMsg = jsonObject.get("text").toString();
+								} catch (JSONException e) {
+									// TODO Auto-generated catch block
+									rtnMsg = "Unhandled Exception Encountered";
+								}
+            			    	if (rtnMsg.equals("CONFIRM")) {
+            			    		rtnMsg = "Your request for payment from account " + contents + " has completed successfully";
+            			    	} else {
+            			    		rtnMsg = "Something fucked up -- Message: " + rtnMsg;
+            			    	}
+            			    	t.setText(rtnMsg);
+            			    }
+
+            			    @Override
+            			    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+            			    	t.setText("Server request failed, please verify your connection status and try again.");
+            			    }
+
+            			    @Override
+            			    public void onRetry(int retryNo) {
+            			        // called when request is retried
+            			    }
+            			});
+            			
+            		} catch(Exception e) {
+            			Log.v("dbg", "failed here: " + e.toString());
+            			e.printStackTrace();
+            		}    	
             } else 
             if (resultCode == RESULT_CANCELED) {
               // Handle cancel
